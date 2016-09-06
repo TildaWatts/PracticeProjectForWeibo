bcf2eb0e7af5c2d71b68310902723d3beef0bf3c
TildaWatts <215722591@qq.com>
2016-09-06 13:52:13 +0800

    初始化项目

---
+++ PracticeProjectForWeibo/PracticeProjectForWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticeProjectForWeibo
+//
+//  Created by Ezra on 16/9/6.
+//  Copyright © 2016年 Ezra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
